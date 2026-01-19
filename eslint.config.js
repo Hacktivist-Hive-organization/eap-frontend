90813aeacb7fb9a32b37ed4eff0ea3b8c7519078
Yuval Hashmonay <yuval@Yuvals-MacBook-Air.local>
2026-01-19 16:02:08 +0100

    yet another rewrite

--- eslint.config.js
+++ eslint.config.js
@@ -1,62 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-  globalIgnores(["dist", "node_modules"]),
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
   {
-    files: ["**/*.{js,ts,jsx,tsx}"],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2026,
+      ecmaVersion: "latest",
       sourceType: "module",
-      globals: globals.browser
+      globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "@typescript-eslint": tsPlugin,
-      import: importPlugin
-    },
-    extends: [
-      js.configs.recommended,
-      "plugin:@typescript-eslint/recommended",
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite
-    ],
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "import/no-relative-parent-imports": "error",
-      "import/order": [
-        "warn",
-        {
-          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-          pathGroups: [
-            {
-              pattern:
-                "{components,pages,services,store,styles,hooks,utils,App,assets}/**",
-              group: "internal"
-            }
-          ],
-          pathGroupsExcludedImportTypes: ["builtin"],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true }
-        }
-      ],
-      "padding-line-between-statements": [
-        "error",
-        { blankLine: "always", prev: "block", next: "*" },
-        { blankLine: "always", prev: "class", next: "*" },
-        { blankLine: "always", prev: "function", next: "*" },
-        { blankLine: "always", prev: "*", next: "return" }
-      ]
-    },
-    settings: {
-      "import/resolver": { typescript: { project: "./tsconfig.json" } }
-    }
-  }
-]);
+  },
+];
